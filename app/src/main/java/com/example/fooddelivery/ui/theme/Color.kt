009ede9057246b20f35cb4560f994b3142dcba4c
WinPear <leminhthang24012006@gmail.com>
2026-04-02 20:11:05 +0700

    style: set up light theme

--- app/src/main/java/com/example/fooddelivery/ui/theme/Color.kt
+++ app/src/main/java/com/example/fooddelivery/ui/theme/Color.kt
@@ -2,10 +2,30 @@ package com.example.fooddelivery.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val md_theme_light_primary = Color(0xFFFF7622)
+val md_theme_light_onPrimary = Color(0xFFFFFFFF) // Color for icon or text on top of primary
+val md_theme_light_primaryContainer = Color(0xFFFFDBC9) // light color background
+val md_theme_light_onPrimaryContainer = Color(0xFF331200)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// color for filter, floating action button
+val md_theme_light_secondary = Color(0xFF775748)
+val md_theme_light_onSecondary = Color(0xFFFFFFFF)
+val md_theme_light_secondaryContainer = Color(0xFFFFDBC9)
+val md_theme_light_onSecondaryContainer = Color(0xFF2B160A)
+
+// color for textFields, textFields error
+val md_theme_light_error = Color(0xFFBA1A1A)
+val md_theme_light_errorContainer = Color(0xFFFFDAD6)
+val md_theme_light_onError = Color(0xFFFFFFFF)
+val md_theme_light_onErrorContainer = Color(0xFF410002)
+
+// color for card
+val md_theme_light_background = Color(0xFFFFF8F6) // main color background
+val md_theme_light_onBackground = Color(0xFF121223) // main color text
+val md_theme_light_surface = Color(0xFFFFFFFF) // for foods
+val md_theme_light_onSurface = Color(0xFF32324D) // text in cards
+
+// color for interval
+val md_theme_light_surfaceVariant = Color(0xFFF4DED4) // background for textField
+val md_theme_light_onSurfaceVariant = Color(0xFFA0A5BA) // text holder
+val md_theme_light_outline = Color(0xFF85736B) // Border
