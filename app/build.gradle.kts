deae7047f920df2a68f872d7b11cd2bcb4b2a075
WinPear <leminhthang24012006@gmail.com>
2026-05-04 22:46:57 +0700

    bugfix: gradle tooling error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,7 +72,6 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.foundation.layout)
-    implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.compose.foundation)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
@@ -85,7 +84,6 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.8.3")
 
     implementation(libs.hilt.android)
-    debugImplementation(libs.androidx.ui.tooling)
     ksp(libs.hilt.compiler)
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
 
