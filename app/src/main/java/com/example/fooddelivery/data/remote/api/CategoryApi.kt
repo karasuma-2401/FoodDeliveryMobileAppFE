b9ffa5e65044c94de8593c4a7966784b316a2668
Huynh Gia Thinh <thinh871779@gmail.com>
2026-06-23 23:18:39 +0700

    fix: type mismatch between FE and BE in category

--- app/src/main/java/com/example/fooddelivery/data/remote/api/CategoryApi.kt
+++ app/src/main/java/com/example/fooddelivery/data/remote/api/CategoryApi.kt
@@ -1,5 +1,6 @@
 package com.example.fooddelivery.data.remote.api
 
+import com.example.fooddelivery.data.remote.dto.BaseListResponse
 import com.example.fooddelivery.data.remote.dto.CategoryResponse
 import retrofit2.Response
 import retrofit2.http.GET
@@ -11,5 +12,5 @@ interface CategoryApi {
         @Query("keyword") keyword: String? = null,
         @Query("limit") limit: Int? = 50,
         @Query("offset") offset: Int? = 0
-    ): Response<List<CategoryResponse>>
+    ): Response<BaseListResponse<CategoryResponse>>
 }
