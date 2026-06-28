3e2d51e8de8b17bc86fc0200ef714416b9cbd8d6
Huynh Gia Thinh <thinh871779@gmail.com>
2026-06-29 03:34:32 +0700

    style: complete ui part 4

--- app/src/main/java/com/example/fooddelivery/ui/screens/restaurant/component/BestSellerItemCard.kt
+++ app/src/main/java/com/example/fooddelivery/ui/screens/restaurant/component/BestSellerItemCard.kt
@@ -4,11 +4,8 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
@@ -65,11 +62,5 @@ fun BestSellerItemCard(item: BestSellerItem) {
             text = item.price,
             color = MaterialTheme.colorScheme.primary
         )
-
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Icon(Icons.Default.Star, contentDescription = null)
-            Spacer(modifier = Modifier.width(4.dp))
-            Text(item.rating.toString())
-        }
     }
 }
